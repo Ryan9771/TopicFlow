4cbb87ec6fa5a2078ebd16cb2a527fd335dd71e1
Ryan <rkp21@ic.ac.uk>
2025-03-28 23:10:42 +0000

    FEAT: Added react flow and simple background variant

--- client/src/pages/Graph.tsx
+++ client/src/pages/Graph.tsx
@@ -1,60 +1,26 @@
-import style from "../util/Styles";
-import 'beautiful-react-diagrams/styles.css';
-import Diagram, { useSchema, createSchema} from 'beautiful-react-diagrams';
+import { ReactFlowProvider } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { ReactFlow, Background, BackgroundVariant } from '@xyflow/react';
+import { Slide } from '../components/Slide';
+ 
+const nodeTypes = {
+  slide: Slide,
+};
 
-const initialSchema = createSchema({
-    nodes: [
-        {
-            id: "1",
-            content: "Node 1",
-            coordinates: [250, 60],
-        },
-        {
-            id: "2",
-            content: "Node 2",
-            coordinates: [100, 200],
-        },
-        {
-            id: "3",
-            content: "Node 3",
-            coordinates: [250, 200]
-        },
-        {
-            id: "4",
-            content: "Node 4",
-            coordinates: [400, 200]
-        }
-    ],
-    links: [
-        {
-            input: '1',
-            output: '2'
-        },
-        {
-            input: '1',
-            output: '3'
-        },
-        {
-            input: '1',
-            output: '4'
-        }
-    ],
-});
+
+const nodes = [
+    { id: '0', type: 'slide', position: { x: 0, y: 0 }, data: {} },
+  ];
 
 
 function Graph() {
-    const [schema, { onChange }] = useSchema(initialSchema);
-
     return (
-        <div className="w-full flex">
-            <Diagram schema={schema} onChange={onChange} />
-        </div>
+        <ReactFlowProvider>
+            <ReactFlow nodes={nodes} nodeTypes={nodeTypes} fitView>
+                <Background variant={BackgroundVariant.Dots} />
+            </ReactFlow>
+        </ReactFlowProvider>
     )
-}
-
-
-const styles = {
-
 }
 
 export default Graph;
